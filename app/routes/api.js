37160a7e960f8e9429673ee99fd8b393431d4ec5
Matthew Dean <deanmatthew16@gmail.com>
2016-11-11 00:51:32 -0500

    Beef up the subject endoint

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,14 +2,31 @@ var express = require('express');
 var router = express.Router();
 var assert = require('assert');
 
-router.get('/courses/:subject', function(req, res, next) {
-    var coll = req.app.get('db').collection('courses');
-    coll.find({number: new RegExp("^" + req.params.subject, 'i')})
-        .sort({number: 1})
-        .toArray(function(err, docs) {
-            assert.equal(null, err);
-            res.json(docs);
-        });
+router.get('/courses/subject/:subject', function(req, res, next) {
+    var findObj = {};
+    var subj = req.params.subject.trim();
+    if (subj.trim() === '' || !validateSubject(subj)) {
+        // Validate the subject
+        res.json(new ApiError('Invalid parameter', subj, 'subject'));
+    } else {
+        var coll = req.app.get('db').collection('courses');
+        coll.find({number: new RegExp("^" + subj, 'i')})
+            .sort({number: 1})
+            .toArray(function(err, docs) {
+                assert.equal(null, err);
+                res.json(docs);
+            });
+    }
 });
+
+function validateSubject(subj) {
+    return /[A-Z]+/i.test(subj);
+}
+
+function ApiError(reason, parameter, value) {
+    this.reason = reason;
+    this.parameter = parameter;
+    this.value = value;
+}
 
 module.exports = router;
