d141b3cfd8386e89af7f79c56026a9c46a8e5fad
Matthew Dean <deanmatthew16@gmail.com>
2016-11-11 01:02:33 -0500

    Option to skip indexing using --no-index
    The site is slow to start up when it has to index many institutions. If the
    data has not changed, --no-index will allow express to spin up almost
    instantaneously.

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ app.use(express.static(path.join(__dirname, 'app/public')));
 const port = process.env.PORT || 8080;
 const mongoUrl = 'mongodb://localhost:27017/novaXfer';
 
+//////////////// COMMAND LINE ARGUMENTS /////////////////
+var doIndex = true;
+process.argv.slice(2).forEach(function(val, index, array) {
+    if (val === '--no-index') {
+        doIndex = false;
+    }
+});
+
+
 //////////////////////// ROUTING ////////////////////////
 app.use('/api', api);
 
@@ -27,9 +36,9 @@ app.use('/api', api);
 ///////////////////// ERROR HANDLING ////////////////////
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Development error handler
@@ -59,28 +68,35 @@ mongodb.MongoClient.connect(mongoUrl, function(err, db) {
     assert.equal(null, err);
     var courses = db.collection('courses');
 
-    // Index all our institutions before we start serving
-    indexers.index(function(equivalency) {
-        courses.updateOne({number: equivalency.vccs.number},
-            {
-                $setOnInsert: {
-                    credits: equivalency.vccs.credits,
-                },
-                // Add to equivalencies array if data already exists
-                $addToSet: {
-                    equivalencies: equivalency.other
-                }
-            },
-            {upsert: true},
-            function(err, result) {
-                assert.equal(null, err);
-            }
-        );
-    }, function() {
+    var start = function() {
         // Finished initializing, start up
         app.set('db', db);
 
         app.listen(port);
-        console.log('Magic happens on port ' + port);
-    })
+        console.log('Magic is happening on port ' + port);
+    }
+
+    if (doIndex) {
+        // Index all our institutions before we start serving
+        indexers.index(function(equivalency) {
+            courses.updateOne({number: equivalency.vccs.number},
+                {
+                    $setOnInsert: {
+                        credits: equivalency.vccs.credits,
+                    },
+                    // Add to equivalencies array if data already exists
+                    $addToSet: {
+                        equivalencies: equivalency.other
+                    }
+                },
+                {upsert: true},
+                function(err, result) {
+                    assert.equal(null, err);
+                }
+            );
+        }, start);
+    } else {
+        console.log('Skipping re-indexing. Courses may not be up to date.');
+        start();
+    }
 });
