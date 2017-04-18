4c6b6f9498c8a32f7e07b139c5e35599864409f2
Matthew Dean <deanmatthew16@gmail.com>
2017-04-17 21:02:59 -0400

    Redirect /api to docs on GitHub

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,18 +1,27 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import v1 from './v1'
+import { Database } from '../../Database';
 
 export default function(): Router {
     const router = Router();
 
-    const modules: (() => [string, Router])[] = [
-        v1
-    ];
+    // Only load API modules if connected to the database. This is useful for
+    // testing so we can test non-API routes without connecting to the database
+    if (Database.get().isConnected()) {
+        const modules: (() => [string, Router])[] = [
+            v1
+        ];
 
-    for (let m of modules) {
-        const data = m();
-        // data[0] is the mount point, data[1] is the Router
-        router.use(data[0], data[1]);
+        for (let m of modules) {
+            const data = m();
+            // data[0] is the mount point, data[1] is the Router
+            router.use(data[0], data[1]);
+        }
     }
+
+    router.get('/', (req: Request, res: Response) => {
+        res.redirect('https://github.com/thatJavaNerd/novaXfer/blob/master/docs/api.md')
+    });
 
     return router;
 }
---
+++ test/routes.test.ts
@@ -0,0 +1,20 @@
+import { Application } from 'express';
+import { createServer } from '../src/server';
+import * as request from 'supertest';
+
+describe('routes', () => {
+    let app: Application;
+
+    before('create app', function() {
+        app = createServer();
+    });
+
+    describe('GET /api', () => {
+        it('should redirect to the docs on GitHub', () =>
+            request(app)
+                .get('/api')
+                .expect(301)
+                .expect('Location', /github\.com/)
+        );
+    });
+});
