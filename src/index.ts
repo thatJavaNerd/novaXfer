41027b7f0a0fbfaa76a8ba5c7f02e98daff79ca9
Matthew Dean <deanmatthew16@gmail.com>
2017-04-18 22:35:02 -0400

    Don't use console.log

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ bootstrap({
 });
 
 async function bootstrap(options: BootstrapOptions) {
-    console.log(colors.bold('Starting novaXfer v' + packageJson.version));
+    log(colors.bold('Starting novaXfer v' + packageJson.version));
 
     try {
         await Database.get().connect(Mode.PROD);
@@ -48,10 +48,10 @@ async function bootstrap(options: BootstrapOptions) {
 
     if (options.forceIndex || await (new MetaDao().shouldIndex())) {
         try {
-            console.log('Indexing all institutions...');
+            log('Indexing all institutions...');
             const report = await doFullIndex();
             const rate = (report.weightedSuccessRate * 100).toFixed(4);
-            console.log(`Indexed ${report.coursesIndexed} equivalencies from ` +
+            log(`Indexed ${report.coursesIndexed} equivalencies from ` +
                 `${report.institutionsIndexed} institutions (${rate}% success rate)`);
 
         } catch (ex) {
@@ -64,7 +64,7 @@ async function bootstrap(options: BootstrapOptions) {
         const app = await createServer();
         logEndpoints(app);
         await app.listen(options.port);
-        console.log('\nMagic is happening on port ' + colors.bold(options.port.toString()));
+        log('\nMagic is happening on port ' + colors.bold(options.port.toString()));
     } catch (ex) {
         // Don't try to handle the error, let it be printed to stderr. We do
         // want to make sure we're disconnected from the database though.
@@ -72,17 +72,16 @@ async function bootstrap(options: BootstrapOptions) {
 
         throw ex;
     }
-
 }
 
 function printHelp() {
     const script = path.basename(__filename);
-    console.log(colors.bold(`${packageJson.name} v${packageJson.version}`));
-    console.log('\nUsage:');
-    console.log(`$ ${script} [--force-index] [--help]`);
-    console.log('  --force-index: Forces fresh data to be imported from the Indexers');
-    console.log('  --help: Prints this help message');
-    console.log(`\nFor more see ${colors.bold(packageJson.repository)}`);
+    log(colors.bold(`${packageJson.name} v${packageJson.version}`));
+    log('\nUsage:');
+    log(`$ ${script} [--force-index] [--help]`);
+    log('  --force-index: Forces fresh data to be imported from the Indexers');
+    log('  --help: Prints this help message');
+    log(`\nFor more see ${colors.bold(packageJson.repository)}`);
 }
 
 /**
@@ -91,11 +90,16 @@ function printHelp() {
  * @param  {object} app Express app
  */
 function logEndpoints(app) {
-    console.log('Available endpoints:\n');
+    log('Available endpoints:\n');
     const endpoints = _.sortBy(listEndpoints(app), (e: any) => e.path);
     for (const e of endpoints) {
-        console.log(`  ${_.join(e.methods, ', ')} ${e.path}`);
+        log(`  ${_.join(e.methods, ', ')} ${e.path}`);
     }
+}
+
+function log(str: string) {
+    process.stdout.write(str);
+    process.stdout.write('\n');
 }
 
 /**
