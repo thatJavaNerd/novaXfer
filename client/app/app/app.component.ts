addf769d60dee761c59cd5ac37bff7e84f04864a
Matthew Dean <deanmatthew16@gmail.com>
2017-04-28 15:58:06 -0400

    Use a more succinct model

--- client/app/app/app.component.ts
+++ client/app/app/app.component.ts
@@ -8,7 +8,8 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
-import { Observable } from 'rxjs/Observable';
+
+import * as _ from 'lodash/lodash';
 
 @Component({
     selector: 'novaxfer',
@@ -17,25 +18,59 @@ import { Observable } from 'rxjs/Observable';
     providers: [ EquivalencyService ]
 })
 export class AppComponent {
-    private static readonly COURSE_PATTERN = / *[A-Z]{3} ?[0-9]{3} */i;
+    private static readonly COURSE_PATTERN = /^ *[A-Z]{3} +[0-9]{3} *$/i;
+    private static readonly NBSP = String.fromCharCode(160);
+
     public input = new FormControl('',
         [
             Validators.required,
             Validators.pattern(AppComponent.COURSE_PATTERN)
         ]);
 
-    public summary: Observable<CourseSummary>;
+    public summary: SuccinctCourseSummary;
 
     public constructor(@Inject(EquivalencyService) private equivService: EquivalencyService) {
-        this.summary = this.input.valueChanges
-            .filter(AppComponent.validateCourse)
+        this.input.valueChanges
+            .filter(() => this.input.valid)
             .debounceTime(300)
             .distinctUntilChanged()
-            .map((course: string) => course.trim().split(' '))
-            .switchMap((parts: string[]) => this.equivService.courseSummary(parts[0], parts[1]));
+            .switchMap((raw: string) => {
+                this.summary = null;
+                const parts = AppComponent.normalizeWhitespace(raw).split(' ');
+                return this.equivService.courseSummary(parts[0].trim(), parts[1].trim());
+            })
+            .map(AppComponent.makeSuccinct)
+            .subscribe((cs: SuccinctCourseSummary) => {
+                this.summary = cs;
+            });
     }
 
-    private static validateCourse(input: string): boolean {
-        return input !== undefined && AppComponent.COURSE_PATTERN.test(input);
+    private static makeSuccinct(cs: CourseSummary): SuccinctCourseSummary {
+        return cs === null ? null : {
+            institutions: AppComponent.formatInstitutions(cs.institutions),
+            course: cs.subject + ' ' + cs.number,
+            exists: cs.exists,
+            icon: cs.exists ? 'check_circle' : 'block',
+            link: cs.exists ? `/course/${cs.subject}/${cs.number}` : null
+        };
     }
+
+    private static formatInstitutions(i: string[]): string {
+        if (i.length === 0) return null;
+        if (i.length === 1) return i[0];
+        if (i.length === 2) return i[0] + ' and ' + i[1];
+        return `${i[0]}, ${i[1]}, and ${i.length - 2} more`;
+    }
+
+    private static normalizeWhitespace(text) {
+        return text.replace(new RegExp(`(?:\r\n|\r|\n|${AppComponent.NBSP}| )+`, 'g'), ' ').trim();
+    }
+}
+
+export interface SuccinctCourseSummary {
+    institutions: string;
+    course: string;
+    exists: boolean;
+    icon: string;
+    link: string;
 }
