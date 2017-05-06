2a7605d6f06bf1b989ed0c189e9a415ccef5626c
Matthew Dean <deanmatthew16@gmail.com>
2017-05-06 19:16:58 -0400

    Get Karma working again

--- client/config/webpack.test.js
+++ client/config/webpack.test.js
@@ -1,2 +1,31 @@
 // Simply re-export the common configuration for now
-module.exports = require('./webpack.common');
+module.exports = {
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: {
+                            configFileName: 'client/config/tsconfig.json'
+                        }
+                    },
+                    'angular2-template-loader'
+                ]
+            },
+            {
+                test: /\.pug$/,
+                loader: ['raw-loader', 'pug-html-loader']
+            },
+            {
+                test: /\.scss$/,
+                loader: 'null-loader'
+            }
+        ]
+    }
+};
